743ce677b3a191852a116f8edda0e96e9434c0cd
rguixaro <rguixaro@protonmail.ch>
2024-03-26 23:12:46 +0100

    add head and header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,75 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import type { Links, Page, Site, Socials } from '@/types'
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+// Global
+export const SITE: Site = {
+	TITLE: 'Astro Sphere',
+	DESCRIPTION:
+		'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
+	AUTHOR: 'Ricard Guixaró',
+}
+
+// Work Page
+export const WORK: Page = {
+	TITLE: 'Work',
+	DESCRIPTION: 'Places I have worked.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+	TITLE: 'Blog',
+	DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+	TITLE: 'Projects',
+	DESCRIPTION: 'Recent projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+	TITLE: 'Search',
+	DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
+// Links
+export const LINKS: Links = [
+	{
+		TEXT: 'Home',
+		HREF: '/',
+	},
+	{
+		TEXT: 'Work',
+		HREF: '/work',
+	},
+	{
+		TEXT: 'Blog',
+		HREF: '/blog',
+	},
+	{
+		TEXT: 'Projects',
+		HREF: '/projects',
+	},
+]
+
+// Socials
+export const SOCIALS: Socials = [
+	{
+		NAME: 'Email',
+		ICON: 'email',
+		TEXT: 'rguixaro@protonmail.ch',
+		HREF: 'mailto:rguixaro@protonmail.ch',
+	},
+	{
+		NAME: 'Github',
+		ICON: 'github',
+		TEXT: 'rguixaro',
+		HREF: 'https://github.com/rguixaro',
+	},
+	{
+		NAME: 'LinkedIn',
+		ICON: 'linkedin',
+		TEXT: 'rguixaro',
+		HREF: 'https://www.linkedin.com/in/rguixaro/',
+	},
+]
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface Page {
+	TITLE: string
+	DESCRIPTION: string
+}
+
+export interface Site extends Page {
+	AUTHOR: string
+}
+
+export type Links = {
+	TEXT: string
+	HREF: string
+}[]
+
+export type Socials = {
+	NAME: string
+	ICON: string
+	TEXT: string
+	HREF: string
+}[]
