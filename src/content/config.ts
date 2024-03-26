5d60371e9e762dfef57afd910144c7101a797cc2
rguixaro <rguixaro@protonmail.ch>
2024-03-26 23:12:20 +0100

    add home page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,9 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { blogSchema } from '@/schemas/blog'
 
 const blog = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+	schema: blogSchema,
 })
 
 export const collections = { blog }
---
+++ src/schemas/blog.ts
@@ -0,0 +1,10 @@
+import { z } from 'astro:content'
+
+export const blogSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	// Transform string to Date object
+	pubDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
+})
