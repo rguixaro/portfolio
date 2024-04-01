9f160d9138b7befa1241888e172c659770d25586
rguixaro <rguixaro@protonmail.ch>
2024-04-01 21:24:35 +0200

    remove old contents

--- src/content/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection } from 'astro:content'
-import { blogSchema } from '@/schemas/blog'
-
-const blog = defineCollection({
-	type: 'content',
-	schema: blogSchema,
-})
-
-export const collections = { blog }
