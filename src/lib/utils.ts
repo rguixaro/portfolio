fe919cd3dff45f4892fcf63d35c1a16f69d06991
rguixaro <rguixaro@protonmail.ch>
2024-07-07 19:15:09 +0200

    fix(styling): update mobile-responsiveness

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,21 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 
-export function formatDate(date: Date) {
-	return Intl.DateTimeFormat('en-US', {
-		month: 'short',
-		day: '2-digit',
-		year: 'numeric',
-	}).format(date)
-}
-
-export function readingTime(html: string) {
-	const textOnly = html.replace(/<[^>]+>/g, '')
-	const wordCount = textOnly.split(/\s+/).length
-	const readingTimeMinutes = (wordCount / 200 + 1).toFixed()
-	return `${readingTimeMinutes} min read`
-}
-
 export function getStack(stack: string[]): TechStack[] {
 	const arr = new Set<TechStack>()
 	stack.forEach((item: string) => {
